c8b27be9a812d1f7ca4dcc4fd2696bd1b661b3e0
lmusse4 <127805935+lmusse4@users.noreply.github.com>
2023-04-06 09:18:42 -0500

    Add files via upload
    New assignment file

--- usersEmail.js
+++ usersEmail.js
@@ -1,15 +1,14 @@
 let emailApp = {
-    name: 'Gmail',
-    mailboxes: [
-      {
-        name: "Inbox",
-        emails: [
+    mailbox: {
+      gmail: {
+        inbox: [
           {
             from: "example1@example.com",
             to: ["you@example.com"],
             subject: "Hi! This is my subject",
             body: "This is also the body email.",
           },
+  
           {
             from: "example2@example.com",
             to: ["you@example.com"],
@@ -17,32 +16,54 @@ let emailApp = {
             body: "Another body email.",
           },
         ],
-      },
-      {
-        name: "Sent",
-        emails: [
-          {
-            from: "you@example.com",
-            to: ["example1@example.com"],
-            subject: "RE: Something Important",
-            body: "Thanks for the email!",
+        drafts: [
+           {
+            to: "person@person.com",
+            subject: "A Draft email!",
+            body: "just checking in",
           },
+      ],
+        sent: [
+          
+             {
+                to: "example3@example.com",
+                subject: "RE: Something Important",
+                body: "Thanks for the email!",
+            },
+          
         ],
+        Contacts: [
+          
+             {
+                name: "John",
+                email: "John@example.com",
+            },
+          
+        ],
+        allmail: [],
       },
-    ],
-    contacts: [
-      {
-        name: "John",
-        email: "John@example.com",
-      },
-      {
-        name: "Kayone",
-        email: "Kayone@example.com",
-      },
-      {
-        name: "Kenny",
-        email: "Kenny@example.com",
-      },
-    ],
+    },
   };
-  
+
+   // Instructions
+  for(let allMailBoxes in emailApp.mailbox.gmail){
+    console.log(allMailBoxes);
+  }
+
+  let inboxEmails = emailApp.mailbox.gmail.inbox;
+  console.log(inboxEmails);
+
+  console.log(emailApp.mailbox.gmail.inbox[1].body);
+
+emailApp.mailbox.gmail.sent[0].sentcheck = "true";
+console.log(emailApp.mailbox.gmail.sent[0].sentcheck);
+
+emailApp.mailbox.gmail.drafts.push({
+    to: "Dear friend",
+    subject: "Miss ya!",
+    body: "We should have a dinner date soon!",
+  });
+
+
+  console.log(emailApp.mailbox.gmail.drafts[1])
+
